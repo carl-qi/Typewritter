0ad4c35a2c598349b1346c07567b32071cb6f434
sang <rkooo567@google.com>
2017-10-07 23:06:26 -0700

    delete game.py for the new game.py

--- app.py
+++ app.py
@@ -7,28 +7,20 @@ SLEEP_TIME = 0.8
 
 class App:
     def __init__(self):
-        pass
+        self.words = []
 
     def start_game(self):
-        self.field = Field(50, 5)
-        words = []
+        self.field = Field(20, 5)
         game_over = False
-        
-        
-        
-        
-        
         num_loops = 0
-        
         while not game_over:
-            os.system('clear')
+            os.system('cls')
             self.field.show_field()
             print('_' * 120)
             if num_loops % (1.6 // SLEEP_TIME) == 0:
                 word = Word(self.field)
-                words.append(word)
-                        
-            for w in words:
+                self.words.append(word)   
+            for w in self.words:
                 if w.speed >= (num_loops) % 3:
                     if (w.row() + w.speed) > (self.field.row_length - 1):
                         self.end_game()
@@ -36,19 +28,9 @@ class App:
                         break
                     else:
                         w.move(w.row() + 1, w.col())
-            
             time.sleep(SLEEP_TIME)
             num_loops += 1
     
-
-
     def end_game(self):
         print('YOU LOSER!')
 
-a = App()
-a.start_game()
-
-"""
-    words[0].remove()
-    words.pop(0)
-"""
